9ccd2eb6102299221fd6b44adcf12aa9d25f4e18
AntonioGM <antoniogutierrez05@ieasamachado.org>
2024-01-11 14:22:49 +0100

    a

---
+++ Javascript/circulos.js
@@ -0,0 +1,9 @@
+function ocultarRojo(a){
+    document.getElementById(a).style.visibility = "hidden" ;
+}
+function ocultarVerde(b){
+    document.getElementById(b).style.visibility = "hidden" ;
+}
+function ocultarAzul(c){
+    document.getElementById(c).style.visibility = "hidden" ;
+}
